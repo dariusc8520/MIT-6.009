b0efb4a90538c84d0df19ce1812ea284ffa11afe
unknown <Darius Chan@DESKTOP-39VED7C.mit.edu>
2021-02-24 13:31:43 -0500

    added comments and doc strings

--- lab1/lab.py
+++ lab1/lab.py
@@ -8,30 +8,36 @@ from PIL import Image as Image
 
 
 def get_pixel(image, x, y):
-    y = y*image['width']
+    """
+    Retrieves the pixel value from image
+    Will keep requested pixel values within the bounds of the image
+    """
+    y = y*image['width'] #Necessary to multiply by width for correct indexing in a list
     
     if x < 0:
         x = 0
-        #print('x 0 edge')
     if y < 0:
         y = 0
-        #print('y 0 edge')
     if x > image['width']-1:
         x =  image['width']-1
-        #print('x max edge')
     if y > (image['height']-1)*image['width']:
         y = (image['height']-1)*image['width']
-        #print('y max edge',y)
-    #print(x+y)
+        
     return image['pixels'][x+y]
 
 
 def set_pixel(image, c):
+    """
+    Should be called when you have an empty list you are updating with new values
+    """
     image['pixels'].append(c)
 
 
 def apply_per_pixel(image, func):
-    result = result = copy(image)
+    """
+    Applies a function to every pixel in an image
+    """
+    result = copy(image)
     for y in range(image['height']):
         for x in range(image['width']):
             color = get_pixel(image, x, y)
@@ -41,6 +47,9 @@ def apply_per_pixel(image, func):
 
 
 def inverted(image):
+    """
+    Inverts every pixel value in an image
+    """
     return apply_per_pixel(image, lambda c: 255-c)
 
 
@@ -66,15 +75,19 @@ def correlate(image, kernel):
     Square (nxn)
     """
     result = result = copy(image)
-    n = int(math.sqrt(len(kernel)))
-    offset = int(n//2)
     
+    n = int(math.sqrt(len(kernel)))
+    offset = int(n//2) #Offset necessary for kernel index range
+    #iterates through all pixels in image
+    #Did not use apply per pixel because I had modifications to the index
     for y in range(image['height']):
         for x in range(image['width']):
             avg_value = 0
-            for i in range(0-offset,offset+1):
-                for j in range(0-offset,offset+1):
-                    color = get_pixel(image, x+j, y+i)
+            #Iterates through indexs of the kernel
+            for j in range(0-offset,offset+1):
+                for i in range(0-offset,offset+1):
+                    #Looks for pixel value with offsets from the kernel
+                    color = get_pixel(image, x+i, y+j)
                     avg_value += kernel[((i+offset)*n)+j+offset]*color
             set_pixel(result, avg_value)
     return result
@@ -90,7 +103,7 @@ def round_and_clip_image(image):
     255 in the output; and any locations with values lower than 0 in the input
     should have value 0 in the output.
     """
-    result = result = copy(image)
+    result = copy(image)
     for value in image['pixels']:
         randc = round(value)
         if randc < 0:
@@ -113,19 +126,23 @@ def blurred(image, n):
     # first, create a representation for the appropriate n-by-n kernel (you may
     # wish to define another helper function for this)
     kernel = blur_kernel(n)
-
     # then compute the correlation of the input image with that kernel
     blurred_image = correlate(image,kernel)
-
     # and, finally, make sure that the output is a valid image (using the
     # helper function from above) before returning it.
-    # print('pre rounding number of pixels:', len(blurred_image['pixels']))
     return round_and_clip_image(blurred_image)
 
 def blur_kernel(n): 
+    """
+    Returns a kernel that is a list with n x n equal values of 1/(n*n)
+    """
     return [1/(n*n)]*(n*n)
 
 def sharpened(image, n):
+    """
+    Returns a sharpened image by taking the the difference between a scaled image and a blurred image
+    Takes in an n value for the blur kernel
+    """
     result = copy(image)
     
     blurred_image = blurred(image,n)
@@ -134,27 +151,31 @@ def sharpened(image, n):
     return round_and_clip_image(result)
 
 def edges(image):
+    """
+    Returns an image after applying a Sobel operator on the image
+    The image will have edges highlighted
+    """
     result = copy(image)
-    
-    kx = [-1, 0, 1,
+    kx = [-1, 0, 1, #Highlights differences in the x direction
           -2, 0, 2,
           -1, 0, 1]
     
-    ky = [-1, -2, -1,
+    ky = [-1, -2, -1, #Highlights differences in the y direction
            0,  0,  0,
            1,  2,  1]
-    
-    ox2 = [value*value for value in correlate(image,kx)['pixels']]
+    #Applies the correlation to the image and squares each value in the pixel list
+    ox2 = [value*value for value in correlate(image,kx)['pixels']] 
     oy2 = [value*value for value in correlate(image,ky)['pixels']]
+    #Rounds the values after taking a squared summation
     result['pixels'] = [round(math.sqrt(ox2_val + oy2_val)) for ox2_val, oy2_val in zip(ox2,oy2)]
     return round_and_clip_image(result)
     
 def copy(image):
-    return {
-        'height': image['height'],
-        'width': image['width'],
-        'pixels': []
-    }
+    """
+    Provides a shallow copy of the image to be worked with
+    Returns a dictionary with the same height, width, and an empty pixels list
+    """
+    return {'height': image['height'], 'width': image['width'], 'pixels': [] }
 
 # HELPER FUNCTIONS FOR LOADING AND SAVING IMAGES
 
@@ -202,46 +223,35 @@ if __name__ == '__main__':
     # code in this block will only be run when you explicitly run your script,
     # and not when the tests are being run.  this is a good place for
     # generating images, etc.
-    #bluegill = load_image('test_images/bluegill.png')
-    #save_image(inverted(bluegill),'test_results/inv_bluegill.png')
     
-    # kernel = [0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #           0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #           1, 0, 0, 0, 0, 0, 0, 0, 0,
-    #           0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #           0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #           0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #           0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #           0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #           0, 0, 0, 0, 0, 0, 0, 0, 0]
+    #Inversion
+    bluegill = load_image('test_images/bluegill.png')
+    save_image(inverted(bluegill),'test_results/inv_bluegill.png')
     
-    # pigbird = load_image('test_images/pigbird.png')
-    # correlated = correlate(pigbird,kernel)
-    # clipped = round_and_clip_image(correlated)
-    # save_image(clipped,'test_results/correlated_pigbird.png')
+    #Correlation
+    kernel = [0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0,
+              1, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0]
     
-    # cat = load_image('test_images/cat.png')
-    # save_image(blurred(cat,5),'test_results/blurred_cat.png')
+    pigbird = load_image('test_images/pigbird.png')
+    correlated = correlate(pigbird,kernel)
+    clipped = round_and_clip_image(correlated)
+    save_image(clipped,'test_results/correlated_pigbird.png')
     
-    # expected = {
-    #     'height': 11,
-    #     'width': 11,
-    #     'pixels': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #                0, 0, 0, 0, 28, 28, 28, 0, 0, 0, 0,
-    #                0, 0, 0, 0, 28, 28, 28, 0, 0, 0, 0,
-    #                0, 0, 0, 0, 28, 28, 28, 0, 0, 0, 0,
-    #                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    #                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    # }
-    # save_image(expected,'test_results/centered_pixel_blur_03.png')
-    
+    #Blurring
+    cat = load_image('test_images/cat.png')
+    save_image(blurred(cat,5),'test_results/blurred_cat.png')
+
+    #Sharpening
     python = load_image('test_images/python.png')
     save_image(sharpened(python,11),'test_results/sharpened_python.png')
     
-    center = load_image('test_images/centered_pixel.png')
-    save_image(edges(center),'test_results/edge_centered_pixel.png')
+    #Edge Detection
+    construct = load_image('test_images/construct.png')
+    save_image(edges(construct),'test_results/edge_construct.png')
