e8836253e31085e8955fa740445b0cdc977b49d0
MilkyChan <dariusc@mit.edu>
2021-03-18 02:10:38 -0400

    Adding lab4

---
+++ lab4/lab.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+from util import read_osm_data, great_circle_distance, to_local_kml_url
+
+# NO ADDITIONAL IMPORTS!
+
+
+ALLOWED_HIGHWAY_TYPES = {
+    'motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'unclassified',
+    'residential', 'living_street', 'motorway_link', 'trunk_link',
+    'primary_link', 'secondary_link', 'tertiary_link',
+}
+
+
+DEFAULT_SPEED_LIMIT_MPH = {
+    'motorway': 60,
+    'trunk': 45,
+    'primary': 35,
+    'secondary': 30,
+    'residential': 25,
+    'tertiary': 25,
+    'unclassified': 25,
+    'living_street': 10,
+    'motorway_link': 30,
+    'trunk_link': 30,
+    'primary_link': 30,
+    'secondary_link': 30,
+    'tertiary_link': 25,
+}
+
+
+def build_auxiliary_structures(nodes_filename, ways_filename):
+    """
+    Create any auxiliary structures you are interested in, by reading the data
+    from the given filenames (using read_osm_data)
+    """
+    return None
+
+
+def find_short_path_nodes(aux_structures, node1, node2):
+    """
+    Return the shortest path between the two nodes
+
+    Parameters:
+        aux_structures: the result of calling build_auxiliary_structures
+        node1: node representing the start location
+        node2: node representing the end location
+
+    Returns:
+        a list of node IDs representing the shortest path (in terms of
+        distance) from node1 to node2
+    """
+    raise NotImplementedError
+
+
+def find_short_path(aux_structures, loc1, loc2):
+    """
+    Return the shortest path between the two locations
+
+    Parameters:
+        aux_structures: the result of calling build_auxiliary_structures
+        loc1: tuple of 2 floats: (latitude, longitude), representing the start
+              location
+        loc2: tuple of 2 floats: (latitude, longitude), representing the end
+              location
+
+    Returns:
+        a list of (latitude, longitude) tuples representing the shortest path
+        (in terms of distance) from loc1 to loc2.
+    """
+    raise NotImplementedError
+
+
+def find_fast_path(aux_structures, loc1, loc2):
+    """
+    Return the shortest path between the two locations, in terms of expected
+    time (taking into account speed limits).
+
+    Parameters:
+        aux_structures: the result of calling build_auxiliary_structures
+        loc1: tuple of 2 floats: (latitude, longitude), representing the start
+              location
+        loc2: tuple of 2 floats: (latitude, longitude), representing the end
+              location
+
+    Returns:
+        a list of (latitude, longitude) tuples representing the shortest path
+        (in terms of time) from loc1 to loc2.
+    """
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    # additional code here will be run only when lab.py is invoked directly
+    # (not when imported from test.py), so this is a good place to put code
+    # used, for example, to generate the results for the online questions.
+    pass
