52ceafb8efa8ef1ef170bf472b82dbdd5f099fbc
dariusc8520 <dariusc@mit.edu>
2021-04-01 22:43:31 -0400

    updated quiz practice

--- q1_practice_a/quiz.py
+++ q1_practice_a/quiz.py
@@ -5,7 +5,33 @@
 ##################################################
 
 def trailing_weighted_average(S, W):
-    raise NotImplementedError
+    s_len = len(S)
+    w_len = len(W)
+    result = []
+    for i in range(s_len):
+        result.append(calc_weighted_avg(S,W,i))
+        #print(result)
+    return result
+    
+def calc_weighted_avg(S,W,i):
+    w_len = len(W)
+    result = 0.0
+    if i == 0:
+        result = sum([S[0]*weight for weight in W])
+    else:
+        weight_index = 0
+        while True:
+            if weight_index/w_len == 1:
+                #print('breaking')
+                break
+            #print('weight index:',weight_index)
+            result += S[i]*W[weight_index]
+            #print('result:',result)
+            weight_index+=1
+            i-=1
+            if i<0:
+                i=0
+    return result
 
 
 ##################################################
@@ -13,7 +39,11 @@ def trailing_weighted_average(S, W):
 ##################################################
 
 def all_consecutives(vals, n):
-    raise NotImplementedError
+    upper_val = max(vals)
+    lower_val = min(vals)
+    len_of_consev = n
+    
+    
 
 
 ##################################################
@@ -25,4 +55,10 @@ def cost_to_consume(seq1, seq2):
 
 
 if __name__ == "__main__":
-	pass
+    #print(trailing_weighted_average([1, 2], [0.8, 0.7]))
+    #returns [1.5, 2.3]
+    print(trailing_weighted_average([1, 5, 6, 7], [1]))
+    #returns [1, 5, 6, 7]
+
+
+	#pass
