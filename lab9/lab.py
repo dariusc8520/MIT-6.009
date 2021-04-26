0467cb10cf4fb293830375ee7a9eb8a1de02ddc0
dariusc8520 <dariusc@mit.edu>
2021-04-26 09:32:04 -0400

    adding lab 9

---
+++ lab9/lab.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+"""6.009 Lab 9: Snek Interpreter"""
+
+import doctest
+# NO ADDITIONAL IMPORTS!
+
+
+###########################
+# Snek-related Exceptions #
+###########################
+
+class SnekError(Exception):
+    """
+    A type of exception to be raised if there is an error with a Snek
+    program.  Should never be raised directly; rather, subclasses should be
+    raised.
+    """
+    pass
+
+
+class SnekSyntaxError(SnekError):
+    """
+    Exception to be raised when trying to evaluate a malformed expression.
+    """
+    pass
+
+
+class SnekNameError(SnekError):
+    """
+    Exception to be raised when looking up a name that has not been defined.
+    """
+    pass
+
+
+class SnekEvaluationError(SnekError):
+    """
+    Exception to be raised if there is an error during evaluation other than a
+    SnekNameError.
+    """
+    pass
+
+
+############################
+# Tokenization and Parsing #
+############################
+
+
+def tokenize(source):
+    """
+    Splits an input string into meaningful tokens (left parens, right parens,
+    other whitespace-separated values).  Returns a list of strings.
+
+    Arguments:
+        source (str): a string containing the source code of a Snek
+                      expression
+    """
+    raise NotImplementedError
+
+
+def parse(tokens):
+    """
+    Parses a list of tokens, constructing a representation where:
+        * symbols are represented as Python strings
+        * numbers are represented as Python ints or floats
+        * S-expressions are represented as Python lists
+
+    Arguments:
+        tokens (list): a list of strings representing tokens
+    """
+    raise NotImplementedError
+
+
+######################
+# Built-in Functions #
+######################
+
+
+snek_builtins = {
+    '+': sum,
+    '-': lambda args: -args[0] if len(args) == 1 else (args[0] - sum(args[1:])),
+}
+
+
+##############
+# Evaluation #
+##############
+
+
+def evaluate(tree):
+    """
+    Evaluate the given syntax tree according to the rules of the Snek
+    language.
+
+    Arguments:
+        tree (type varies): a fully parsed expression, as the output from the
+                            parse function
+    """
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    # code in this block will only be executed if lab.py is the main file being
+    # run (not when this module is imported)
+
+    # uncommenting the following line will run doctests from above
+    # doctest.testmod()
+
+    pass
